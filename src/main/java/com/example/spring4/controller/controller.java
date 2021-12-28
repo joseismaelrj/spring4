d348586da9fb4ecb4b468379a0be55bf968c89af
joseismaelrj <jose.nascimento@mercadolivre.com>
2021-12-28 11:53:32 -0300

    Criação das pastas de trabalho.

---
+++ src/main/java/com/example/spring4/controller/controller.java
@@ -0,0 +1,10 @@
+package com.example.spring4.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurante")
+public class controller {
+}
